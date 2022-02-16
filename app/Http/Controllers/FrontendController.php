6276d94af241b4b89b56e5515a42c4d544afa593
Abdurashid <abdurashiddev@gmail.com>
2022-02-16 14:29:51 +0500

    seo

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -75,18 +75,18 @@ class FrontendController extends Controller
     public function new($id)
     {
         $new = Blog::with('category')->where('is_active', 1)->findOrFail($id);
-        SEOMeta::setTitle($new->getValue('title'));
-        SEOMeta::setDescription(Str::limit($new->getValue('content'), 150));
+        SEOMeta::setTitle('Yangiliklar');
+        SEOMeta::setDescription('Xavfsiz daryo unitar korxonasining rasmiy sayti');
         SEOMeta::setCanonical('https://xdduk.uz');
 
-        OpenGraph::setDescription(Str::limit($new->getValue('content'), 150));
-        OpenGraph::setTitle($new->getValue('title'));
+        OpenGraph::setDescription('Xavfsiz daryo unitar korxonasining rasmiy sayti');
+        OpenGraph::setTitle('Yangiliklar');
         OpenGraph::setUrl('https://xdduk.uz');
         OpenGraph::addProperty('type', 'articles');
         OpenGraph::addImage(asset($new->image));
 
-        JsonLd::setTitle($new->getValue('title'));
-        JsonLd::setDescription(Str::limit($new->getValue('content'), 150));
+        JsonLd::setTitle('Yangiliklar');
+        JsonLd::setDescription('Xavfsiz daryo unitar korxonasining rasmiy sayti');
 
         $menus = Menu::where('is_active', 1)->orderBy('order')->get();
         $links = Link::where('is_active', 1)->get();
